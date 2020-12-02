516fe56bdfb54e9a5a5732b5f42aae181d98bf05
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-03 00:17:05 +0600

    loop_prac6 added

---
+++ C/loop/loop_prac6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+int i;
+for (i=1;i<=100;i++)
+
+{
+    printf("%d",i);
+    printf("------------\n");
+    printf("hello world i am new\n");
+    printf("I want to do this\n");
+    printf("-------------\n");
+
+}
+
+
+}
