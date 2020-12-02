3170180f03173d2688972c34e7a6b32ea90bd1ad
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-02 13:36:42 +0600

    loop prac2.c

---
+++ C/loop/loop prac 2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=400;i++)
+    {
+        printf("Sometime you have to play a role of a fool to fool the fool who think they are fooling you\n");
+    }
+}
