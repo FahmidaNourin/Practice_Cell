fd96e7885eb637481d9c490c8915c8e6f272295c
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-03 01:27:23 +0600

    edited

--- C/loop/loop_prac7.c
+++ C/loop/loop_prac7.c
@@ -1,14 +1,15 @@
+// 1+2+3+4+...+n
 #include<stdio.h>
 int main()
 {
-    int i,sum=0;
-    for(i=1;i<=99;i++)
-    {
-    sum=sum+1;
-    }
+   int i,n,sum=0;
+   scanf("%d",&n);
+
+   for(i=1;i<=n;i++){
+    sum=sum+i;
+   }
+
     printf("%d",sum);
-
-
 
 
 }
