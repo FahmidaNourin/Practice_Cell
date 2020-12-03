017b01cbd8ca87ce9fbe62da9d3746b421502a66
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-04 00:12:06 +0600

    loop_hw3.c added

--- C/loop/loop_hw3.c
+++ C/loop/loop_hw3.c
@@ -1,1 +1,12 @@
-
+#include<stdio.h>
+int main()
+{
+    int i,n,sum;
+    scanf("%d",&n);
+    for(i=3;i<=n;i=i+3)
+    {
+        sum=sum+(i*i);
+        printf("%d\n",i);
+    }
+    printf("%d",sum);
+}
