ef331676d0ddd29328269376193b12c8c54e2c88
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-04 23:55:39 +0600

    variable updated

--- C/loop/loop_hw4.c
+++ C/loop/loop_hw4.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int i,M=5000;
+    int i,m=5000;
     for(i=1;i<=380;i++)
     {
-        M=M-4;
+        m=m-4;
     }
-    printf("%d",M);
+    printf("%d",m);
 }
