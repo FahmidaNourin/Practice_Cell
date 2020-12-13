a8f2b4ba149487620c33140623abc20a8719b47d
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-13 22:35:33 +0600

    loop_year.c

--- C/loop/ThePrime.c
+++ C/loop/ThePrime.c
@@ -1,1 +1,12 @@
-
+//The_Prime_Number
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=2;i<=;i=i+1)
+    {
+       if(i%2==0){
+        printf("%d\n",i);
+       }
+    }
--- C/loop/loop_hw9.c
+++ C/loop/loop_hw9.c
@@ -2,8 +2,10 @@
 int main()
 {
     int i,n;
-    for(i=2;i<=300;i=i+2)
+    for(i=1;i<=300;i=i+1)
     {
-       printf("%d\n",i);
+       if(i%2==0){
+        printf("%d\n",i);
+       }
     }
 }
