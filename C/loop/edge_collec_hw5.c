0716dca65f7fbe65fb021071292338c03c819a08
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-16 00:08:55 +0600

    edited

--- C/loop/edge_collec_hw5.c
+++ C/loop/edge_collec_hw5.c
@@ -5,9 +5,9 @@ int main()
     int i,n,cube=1;
     printf("Enter the value of n:");
     scanf("%d",&n);
-    for(i=1; i<=5; i++)
+    for(i=1; i<=n; i++)
     {
       cube=pow(i,3);
-      printf("Number is:%d and result is:%%d\n"i,cube);
+      printf("Number is:%d and result of the %d is:%d\n",i,i,cube);
     }
 }
--- C/loop/edge_collec_hw6.c
+++ C/loop/edge_collec_hw6.c
@@ -4,7 +4,7 @@ int main()
     int i,n,multi=1;
     printf("Enter the value of n");
     scanf("%d",&n);
-    for(i=1;i<=10;i++)
+    for(i=1;i<=n;i++)
     {
         multi=n*i;
         printf("%d*%d=%d\n",n,i,multi);
