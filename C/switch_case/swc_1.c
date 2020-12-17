4cdfb37db3fda1b715b4f9b67dd56d3766099fc9
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-18 01:06:33 +0600

    added

---
+++ C/switch_case/swc_1.c
@@ -0,0 +1,18 @@
+
+// Following is a simple C program
+// to demonstrate syntax of switch.
+#include <stdio.h>
+int main()
+{
+int x;
+scanf("%d",&x);
+
+switch (x%2)
+{
+	case 0: printf("The number is even");
+			break;
+	case 1: printf("The number is odd");
+				break;
+}
+return 0;
+}
