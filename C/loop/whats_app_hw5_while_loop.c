c2f15477b94f07888071268dd66242afe3625a33
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-18 00:32:52 +0600

    added

--- C/loop/whats_app_hw5_while_loop.c
+++ C/loop/whats_app_hw5_while_loop.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,sum1=0,sum2=0;
+    printf("enter the value of n: ");
+    scanf("%d",&n);
+    i=1;
+    while(i<=n)
+    {
+       if(i%2!=0)
+        {
+            sum1=sum1+i;
+        }
+        else
+            {
+                sum2=sum2+i;
 
+        } i++;
+
+    }
+    printf("%d %d is the value",sum1,sum2);
+
+
+}
