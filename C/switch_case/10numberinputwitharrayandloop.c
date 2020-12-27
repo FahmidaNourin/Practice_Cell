0a8a617f9da96d5cedc6019c44bc2cbb2cf4eb41
FahmidaNourin <75219480+FahmidaNourin@users.noreply.github.com>
2020-12-28 02:23:30 +0600

    arrayandloop

---
+++ C/switch_case/10numberinputwitharrayandloop.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main(){
+
+int a[10],i;
+
+// a[0],a[1],a[2].....a[9]
+
+for(i=0;i<=9;i++)
+{
+    scanf("%d",&a[i]);
+}
+
+
+for(i=0;i<=9;i++)
+{
+    printf("%d ",a[i]);
+}
+
+
+}
--- C/switch_case/primeNumberOrNot.c
+++ C/switch_case/primeNumberOrNot.c
@@ -21,8 +21,4 @@ int main()
         printf("The number is not prime");
     }
 
-
-
-
-
 }
